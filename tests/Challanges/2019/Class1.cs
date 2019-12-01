efaf76287bf3f156a32f3344be2359113afacf81
Jimmy <jimmy.mattsson@schotte.se>
2019-12-01 13:10:18 +0100

    Added tests

---
+++ tests/Challanges/2019/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tests.Challanges._2019
+{
+    [TestFixture]
+    public class Day1Test
+    {
+        [Test]
+        public void Nothing()
+        {
+            Assert.Fail();
+        }
+    }
+}
